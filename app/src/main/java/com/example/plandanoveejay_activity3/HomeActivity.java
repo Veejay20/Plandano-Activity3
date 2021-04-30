5580d58b851f9775a8e17e92b2d02f7a48452ef9
Veejay20 <vjplandano@gmail.com>
2021-04-30 23:47:27 +0800

    Initial commit

---
+++ app/src/main/java/com/example/plandanoveejay_activity3/HomeActivity.java
@@ -0,0 +1,14 @@
+package com.example.plandanoveejay_activity3;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate (Bundle saveInstanceState){
+        super.onCreate(saveInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
